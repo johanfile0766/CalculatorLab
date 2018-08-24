b85c938fee9670c0af34252398c66ec0c8c3ea1e
johan  file <35362654+johanfile0766@users.noreply.github.com>
2018-08-24 15:37:52 +0700

    Add files via upload

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -16,5 +16,190 @@ namespace CPE200Lab1
         {
             InitializeComponent();
         }
+        double number;
+        double result;
+        string symbol;
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (lblDisplay.Text == "0" && btn.Text != ".")
+            {
+                lblDisplay.Text = "";
+            }
+            if (order == "clear")
+            {
+                lblDisplay.Text = "";
+                lblDisplay.Text = btn.Text;
+                order = " ";
+            }
+            else if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + btn.Text;
+            }
+        }  // all num
+
+
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = null;
+            lblDisplay.Text = "0";
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+
+            number = float.Parse(lblDisplay.Text);
+            lblDisplay.Text = "0";
+            symbol = "+";
+
+            // result = result + float.Parse(lblDisplay.Text);
+            // lblDisplay.Text = result.ToString();
+            // result = 0;
+        }
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            number = float.Parse(lblDisplay.Text);
+            lblDisplay.Text = "0";
+            symbol = "-";
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            number = float.Parse(lblDisplay.Text);
+            lblDisplay.Text = "0";
+            symbol = "X";
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            number = float.Parse(lblDisplay.Text);
+            lblDisplay.Text = "0";
+            symbol = "/";
+        }
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            number = float.Parse(lblDisplay.Text);
+            lblDisplay.Text = "0";
+            symbol = "%";
+        }
+
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+
+
+            if (symbol == "+")
+            {
+                if (lblDisplay.Text.Length < 8)
+                {
+                    result = (number + float.Parse(lblDisplay.Text));
+                    if (result >= 1e8)
+                    {
+                        lblDisplay.Text = "error";
+                    }
+                    else
+                    {
+                        lblDisplay.Text = Convert.ToString(result);
+                        number = 0;
+                    }
+                }
+                if (lblDisplay.Text.Length > 8)
+                {
+                    lblDisplay.Text = "error";
+                }
+            }
+            if (symbol == "-")
+            {
+                result = (number - float.Parse(lblDisplay.Text));
+                if (result < 0)
+                {
+                    lblDisplay.Text = "error";
+                }
+                else
+                {
+                    lblDisplay.Text = Convert.ToString(result);
+                    number = 0;
+                }
+                if (symbol == "X")
+                {
+                    if (lblDisplay.Text.Length < 8)
+                    {
+                        result = (number * float.Parse(lblDisplay.Text));
+                        if (result >= 1e8)
+                        {
+                            lblDisplay.Text = "error";
+                        }
+                        else
+                        {
+                            lblDisplay.Text = Convert.ToString(result);
+                            number = 0;
+                        }
+                    }
+
+                }
+                if (symbol == "/")
+                { if (lblDisplay.Text == "0")
+                    {
+                        lblDisplay.Text = "error";
+                         }
+                    else {
+                        result = (number / float.Parse(lblDisplay.Text));
+                    }
+                    lblDisplay.Text = Convert.ToString(result);
+                    number = 0;
+                }
+
+                if (symbol == "%")
+                {
+                    result = ((number * float.Parse(lblDisplay.Text)) / 100);
+                    lblDisplay.Text = Convert.ToString(result);
+                    number = 0;
+                }
+            }
+        }
+      
+       
+        
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "0.";
+
+            }
+            if (symbol == ".")
+            {
+                lblDisplay.Text = lblDisplay.Text;
+            }
+            else
+            {
+                lblDisplay.Text = lblDisplay.Text + ".";
+                symbol = ".";
+            }
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 }
